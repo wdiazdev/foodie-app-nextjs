2faf83f4d794116ec22e448815b58f360ef954b0
Wilfredo Diaz <wdiazdev@gmail.com>
2025-09-16 21:22:19 -0400

    meals details page created

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,40 @@
-export default async function MealsDetailsPage({ params }) {
-  const { mealSlug } = await params
+import Image from "next/image"
+import styles from "./page.module.css"
+import { getMeal } from "@/lib/meals"
+import { notFound } from "next/navigation"
+
+export default function MealsDetailsPage({ params }) {
+  const { mealSlug } = params
+
+  const meal = getMeal(mealSlug)
+
+  if (!meal) {
+    notFound()
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />")
   return (
-    <div>
-      <h1>MealsDetails Page</h1>
-      <span>{mealSlug}</span>
-    </div>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   )
 }
